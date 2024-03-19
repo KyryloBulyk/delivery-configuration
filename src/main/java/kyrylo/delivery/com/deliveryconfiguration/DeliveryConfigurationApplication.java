d4c04f10b33850780786ec95b9f2eed44924c552
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-19 19:24:08 +0100

    Initialize Delivery Configuration Server

--- src/main/java/kyrylo/delivery/com/deliveryconfiguration/DeliveryConfigurationApplication.java
+++ src/main/java/kyrylo/delivery/com/deliveryconfiguration/DeliveryConfigurationApplication.java
@@ -2,7 +2,9 @@ package kyrylo.delivery.com.deliveryconfiguration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class DeliveryConfigurationApplication {
 

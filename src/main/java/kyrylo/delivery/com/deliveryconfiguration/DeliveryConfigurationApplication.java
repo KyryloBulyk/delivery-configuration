f0591cb10d19103162ee1c9a6b56fd039b213a08
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-03-19 19:09:19 +0100

    Initilize project

---
+++ src/main/java/kyrylo/delivery/com/deliveryconfiguration/DeliveryConfigurationApplication.java
@@ -0,0 +1,13 @@
+package kyrylo.delivery.com.deliveryconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyrylo/delivery/com/deliveryconfiguration/DeliveryConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package kyrylo.delivery.com.deliveryconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
